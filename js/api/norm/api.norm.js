b49392293b2d933a1dac69b31d5bcde139404449
何帅 <18310335027@163.com>
2017-06-21 17:20:33 +0800

    修改离职识别分析和个二级页面判断颜色方式

--- js/api/norm/api.norm.js
+++ js/api/norm/api.norm.js
@@ -1,31 +1,31 @@
 var apiReq = {};
+var warningValueNum = 0;
 
 function normApi(json) {
-    CTSI_API_JSON = JsonParse(json);
-    apiReq = getArea();
-    apiReq.date = CTSI_API_JSON.months[0].replace('.', '');
-    specificationIndex();
+  CTSI_API_JSON = JsonParse(json);
+  apiReq = getArea();
+  apiReq.date = CTSI_API_JSON.months[0].replace('.', '');
+  warningValue();
 }
 
 /**
  * 2.1.40    用工规范指数.
  */
 function specificationIndex() {
-
-    api('/empNorm/specificationIndex', 3001, 'cbSpecificationIndex', JSON.stringify(apiReq));
-
+  api('/empNorm/specificationIndex', 3001, 'cbSpecificationIndex',
+      JSON.stringify(apiReq));
 }
 /**
  * 2.1.41    用工增岗指数.
  */
 function zengGangIndex() {
-    api('/empNorm/zengGangIndex', 3001, 'cbZengGangIndex', JSON.stringify(apiReq));
-
+  api('/empNorm/zengGangIndex', 3001, 'cbZengGangIndex',
+      JSON.stringify(apiReq));
 }
 /**
  * 2.1.42    用工离岗指数.
  */
 function demobilizedIndex() {
-    api('/empNorm/demobilizedIndex', 3001, 'cbDemobilizedIndex', JSON.stringify(apiReq));
-
+  api('/empNorm/demobilizedIndex', 3001, 'cbDemobilizedIndex',
+      JSON.stringify(apiReq));
 }
