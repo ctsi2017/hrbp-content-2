c00fb7bcbb6d37f3eeb6ed61e3900a3c2a394c4b
何帅 <18310335027@163.com>
2017-06-27 13:19:43 +0800

    变回之前异常变动

--- js/api/flowAnaly/postUnusual/api.post.unusual.js
+++ js/api/flowAnaly/postUnusual/api.post.unusual.js
@@ -1,26 +1,24 @@
-var apiReq1 = {};
-var apiReq2 = {};
+var apiReq = {};
 var warningValueNum = 0;
 
 function postUnusualApi(json) {
     CTSI_API_JSON = JsonParse(json);
     getHead(CTSI_API_JSON, postUnusualClick, true);
-    apiReq1 = getApiReq(true);
-    apiReq2 = getApiReq(true);
+    apiReq = getApiReq(true);
     // 2.1.12	员工在岗情况整体占比图
-    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq1));
+    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq));
 
 }
 function postAbnormalRanking(postAbnormalDuration) {
     // 在岗异常时长	postAbnormalDuration	必填	String
-    apiReq1.postAbnormalDuration = postAbnormalDuration.replace('小时', '');
+    apiReq.postAbnormalDuration = postAbnormalDuration.replace('小时', '');
     // 2.1.13	在岗异常情况排名表
-    api('/mobilityAnalysis/postAbnormalRanking', 3001, 'cbPostAbnormalRanking', JSON.stringify(apiReq1));
+    api('/mobilityAnalysis/postAbnormalRanking', 3001, 'cbPostAbnormalRanking', JSON.stringify(apiReq));
 }
 function postUnusualClick(region2, region3, post, month) {
-    apiReq2 = clickApi(region2, region3, post, month, true);
+    apiReq = clickApi(region2, region3, post, month, true);
     // 2.1.12	员工在岗情况整体占比图
-    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq2));
+    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq));
 }
 function formDataDay(day) {
     postUnusualClick(getItem("getArea"), getItem("getArea1"), getItem("getPost"), day);
