ffcb986f6cd9ac12ce6347b83256cd37cb5b24e9
何帅 <18310335027@163.com>
2017-06-27 10:22:37 +0800

    点击全景视图弹出alert

--- js/map/map.js
+++ js/map/map.js
@@ -125,7 +125,8 @@ function drop(data) { //这是获取点的函数 传递data数据进入后渲染
 
                 }
             }
-            alert(phone, post);
+            alert(coordin+ "--"+position.lng + "," + position.lat);
+            alert(post);
             viewEmpDetailsCB(phone, post);
             $(".map-popframe").show();
             //此处点击弹出个人详情框，可在此发送请求取出数据
