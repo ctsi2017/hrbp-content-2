07d34b8fbde99c2ef2f746b2a5564285b84eae9c
何帅 <18310335027@163.com>
2017-06-27 14:45:41 +0800

    修改在岗异常排名

--- js/api/flowAnaly/postUnusual/api.post.unusual.callback.js
+++ js/api/flowAnaly/postUnusual/api.post.unusual.callback.js
@@ -1,64 +1,66 @@
 function cbEmpPostOverallSituation(json) {
-    json = JsonParse(json);
-    if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
-        removePoint('h-unusprog-point', 'r-h-unusprog');
-        var y_proportion = 0;
-        var n_proportion = 0;
+  json = JsonParse(json);
+  if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
+    removePoint('h-unusprog-point', 'r-h-unusprog');
+    var y_proportion = 0;
+    var n_proportion = 0;
 
-        $.each(json.data, function (i, item) {
-            // 异常
-            $.each(item.inPostYesRate, function (i, iptr) {
-                $('#y_totalNum').html(iptr.totalNum);
-                y_proportion = substringChar(iptr.proportion);
-            });
-            // 稳定
-            $.each(item.inPostNoRate, function (i, ipnr) {
-                $('#n_totalNum').html(ipnr.totalNum);
-                n_proportion = substringChar(ipnr.proportion);
-            })
-        });
-        scaleMap2('h-unusprog', 'base-llwig', 'base-zlwig', y_proportion, n_proportion, 'num-lchar', 'num-zchar');
-        warningValue();
-    }
-    if (checkCodeEqualsOther(json)) {
-        checkDivIfExist('h-unusprog-point', 'r-h-unusprog');
-        warningValue();
-    }
+    $.each(json.data, function (i, item) {
+      // 异常
+      $.each(item.inPostYesRate, function (i, iptr) {
+        $('#y_totalNum').html(iptr.totalNum);
+        y_proportion = substringChar(iptr.proportion);
+      });
+      // 稳定
+      $.each(item.inPostNoRate, function (i, ipnr) {
+        $('#n_totalNum').html(ipnr.totalNum);
+        n_proportion = substringChar(ipnr.proportion);
+      })
+    });
+    scaleMap2('h-unusprog', 'base-llwig', 'base-zlwig', y_proportion,
+        n_proportion, 'num-lchar', 'num-zchar');
+    warningValue();
+  }
+  if (checkCodeEqualsOther(json)) {
+    checkDivIfExist('h-unusprog-point', 'r-h-unusprog');
+    warningValue();
+  }
 }
 
 function cbWarningValue(json) {
-    json = JsonParse(json);
-    if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
-        removePoint('warningValue_point', 'r_warningValue');
-        $.each(json.data, function (i, item) {
-            warningValueNum = item.warningValue;
-            $('#warningValue').html(warningValueNum);
-        });
-        postAbnormalRanking($('#post_abnormal_ranking').html());
-    }
-    if (checkCodeEqualsOther(json)) {
-        checkDivIfExist('warningValue_point', 'r_warningValue');
-        postAbnormalRanking($('#post_abnormal_ranking').html());
-    }
+  json = JsonParse(json);
+  if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
+    removePoint('warningValue_point', 'r_warningValue');
+    $.each(json.data, function (i, item) {
+      warningValueNum = item.warningValue;
+      $('#warningValue').html(warningValueNum);
+    });
+    postAbnormalRanking($('.post-posttime').children('span').html());
+  }
+  if (checkCodeEqualsOther(json)) {
+    checkDivIfExist('warningValue_point', 'r_warningValue');
+    postAbnormalRanking($('.post-posttime').children('span').html());
+  }
 }
 
 function cbPostAbnormalRanking(json) {
-    json = JsonParse(json);
-    if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
-        removePoint('warningValue_point', 'r_warningValue');
-        var dataName = [];
-        var data = [];
-        var warningValueArr = [];
-        var totlNum = [];
-        $.each(json.data, function (i, item) {
-            dataName[i] = item.region ; // 区域\n区域人数+ '\n\n' + Number(item.totalNum)
-            totlNum[i] = item.totalNum + '人';
-            data[i] = getProportion(item.abnormalNum, item.totalNum);
-            warningValueArr[i] = $('#warningValue').html();
-        });
-        changeHColumarWarningValue('post-norchart', dataName, data, warningValueArr,totlNum, '异常');
-    }
-    if (checkCodeEqualsOther(json)) {
-        checkDivIfExist('warningValue_point', 'r_warningValue');
-    }
+  json = JsonParse(json);
+  if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
+    removePoint('warningValue_point', 'r_warningValue');
+    var dataName = [];
+    var data = [];
+    var warningValueArr = [];
+    var totlNum = [];
+    $.each(json.data, function (i, item) {
+      dataName[i] = item.region; // 区域\n区域人数+ '\n\n' + Number(item.totalNum)
+      totlNum[i] = item.totalNum + '人';
+      data[i] = getProportion(item.abnormalNum, item.totalNum);
+      warningValueArr[i] = $('#warningValue').html();
+    });
+    changeHColumarWarningValue('post-norchart', dataName, data, warningValueArr,
+        totlNum, '异常');
+  }
+  if (checkCodeEqualsOther(json)) {
+    checkDivIfExist('warningValue_point', 'r_warningValue');
+  }
 }
