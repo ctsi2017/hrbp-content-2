a37b23b31bec79a9eeba6bd8ceb2b26d5fa4b675
何帅 <18310335027@163.com>
2017-06-27 13:16:23 +0800

    修改点击在岗异常点击发送请求参数

--- js/api/flowAnaly/postUnusual/api.post.unusual.js
+++ js/api/flowAnaly/postUnusual/api.post.unusual.js
@@ -8,7 +8,7 @@ function postUnusualApi(json) {
     apiReq1 = getApiReq(true);
     apiReq2 = getApiReq(true);
     // 2.1.12	员工在岗情况整体占比图
-    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq));
+    api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq1));
 
 }
 function postAbnormalRanking(postAbnormalDuration) {
