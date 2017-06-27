67b8656ba89327628853c1424cde9b413f9b20a2
何帅 <18310335027@163.com>
2017-06-27 13:13:04 +0800

    修改点击在岗异常点击发送请求参数

--- js/api/flowAnaly/postUnusual/api.post.unusual.js
+++ js/api/flowAnaly/postUnusual/api.post.unusual.js
@@ -1,22 +1,24 @@
-var apiReq = {};
+var apiReq1 = {};
+var apiReq2 = {};
 var warningValueNum = 0;
 
 function postUnusualApi(json) {
     CTSI_API_JSON = JsonParse(json);
     getHead(CTSI_API_JSON, postUnusualClick, true);
-    apiReq = getApiReq(true);
+    apiReq1 = getApiReq(true);
+    apiReq2 = getApiReq(true);
     // 2.1.12	员工在岗情况整体占比图
     api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq));
 
 }
 function postAbnormalRanking(postAbnormalDuration) {
     // 在岗异常时长	postAbnormalDuration	必填	String
-    apiReq.postAbnormalDuration = postAbnormalDuration.replace('小时', '');
+    apiReq1.postAbnormalDuration = postAbnormalDuration.replace('小时', '');
     // 2.1.13	在岗异常情况排名表
     api('/mobilityAnalysis/postAbnormalRanking', 3001, 'cbPostAbnormalRanking', JSON.stringify(apiReq));
 }
 function postUnusualClick(region2, region3, post, month) {
-    apiReq = clickApi(region2, region3, post, month, true);
+    apiReq2 = clickApi(region2, region3, post, month, true);
     // 2.1.12	员工在岗情况整体占比图
     api('/mobilityAnalysis/empPostOverallSituation', 3001, 'cbEmpPostOverallSituation', JSON.stringify(apiReq));
 }
